abfd5eb888c73d14dbbbf387233759018f779067
surlejonathan <surle.jonathan@gmail.com>
2021-07-26 11:26:55 +0200

    Informations form

--- src/screens/informations/Informations.tsx
+++ src/screens/informations/Informations.tsx
@@ -1,14 +1,54 @@
-import React, {FC} from 'react';
+import React, {FC, useState, useRef} from 'react';
 import {Text, View} from 'react-native';
+import {Input} from 'react-native-elements';
 
 import {styles} from './InformationsStyles';
 
 interface Props {}
 
 const Informations: FC = (props: Props) => {
+  const [lastName, setLastName] = useState<string | null>(null);
+  const [firstName, setFirstName] = useState<string | null>(null);
+  const [birthday, setBirthday] = useState<Date | null>(null);
+  const lastNameRef = useRef(null);
+  const firstNameRef = useRef(null);
+  const birthdayRef = useRef(null);
+
   return (
     <View style={styles.container}>
-      <Text>Informations screen</Text>
+      <View style={styles.form}>
+        <Input
+          ref={lastNameRef}
+          label="Nom"
+          onSubmitEditing={() => firstNameRef.current.focus()}
+          blurOnSubmit={false}
+          value={lastName}
+          onChangeText={e => setLastName(e)}
+          returnKeyType="next"
+          returnKeyLabel="Suivant"
+          inputStyle={styles.input}
+        />
+        <Input
+          disabled={!lastName}
+          ref={firstNameRef}
+          label="Prénom"
+          inputStyle={styles.input}
+          blurOnSubmit={false}
+          onSubmitEditing={() => birthdayRef.current.focus()}
+          value={firstName}
+          onChangeText={e => setFirstName(e)}
+          returnKeyType="next"
+          returnKeyLabel="Suivant"
+        />
+        <Input
+          disabled={!firstName}
+          ref={birthdayRef}
+          label="Date de naissance"
+          inputStyle={styles.input}
+          placeholder="JJ-MM-AAAA"
+          value={birthday}
+        />
+      </View>
     </View>
   );
 };
--- src/screens/informations/InformationsStyles.tsx
+++ src/screens/informations/InformationsStyles.tsx
@@ -2,8 +2,19 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 12,
+    paddingVertical: 16,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+
+  form: {
+    paddingHorizontal: 10,
+    paddingTop: 12,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+
+  input: {
+    color: 'black',
   },
 });
